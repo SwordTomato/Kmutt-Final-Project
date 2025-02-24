bac717d614ce43031d821d7e9220cca7b6561f52
SwordTomato <122582074+SwordTomato@users.noreply.github.com>
2025-02-24 15:48:04 +0700

    Make Main Info Start

---
+++ js/Info.js
@@ -0,0 +1,15 @@
+document.getElementById('howToPlayBtn').addEventListener('click', function() {
+    window.location.href = 'HTP.html';
+});
+
+document.getElementById('cardListBtn').addEventListener('click', function() {
+    window.location.href = 'CardList.html';
+});
+
+document.getElementById('acdListBtn').addEventListener('click', function() {
+    window.location.href = 'AcdList.html';
+});
+
+document.getElementById('backBtn').addEventListener('click', function() {
+    window.location.href = 'Main.html';
+});
