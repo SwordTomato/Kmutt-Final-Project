e760bce03567a68d1fc1fa58d258cc609c35dbd4
SwordTomato <122582074+SwordTomato@users.noreply.github.com>
2025-03-01 05:09:31 +0700

    Main Website Code

--- js/Start.js
+++ js/Start.js
@@ -1,3 +1,7 @@
+document.getElementById('startBtn').addEventListener('click', function() {
+    window.location.href = 'Play1.html';
+});
+
 document.getElementById('backBtn').addEventListener('click', function() {
     window.location.href = 'Main.html';
 });
